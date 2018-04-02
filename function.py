4fc8e49ecb6393bcaf3fde1387fccae8bc4e539f
Kanardheure <brehier.colin@gmail.com>
2018-04-02 10:59:49 +0200

    Ajout de la barre latéral et des images 90x90 et redimension des images

--- function.py
+++ function.py
@@ -18,46 +18,6 @@ aiguille.set_colorkey(WHITE)
 tube.set_colorkey(WHITE)
 
 
-def draw_rect(window, color, x, y):
-    pygame.draw.rect(window, color, [x, y, 30, 30])
-
-
-def random_rect(window):
-    color = random.randint(0,4)
-    if color == 0:
-        color = BLACK
-    elif color == 1:
-        color = RED
-    elif color == 2:
-        color = GREEN
-    elif color == 3:
-        color = WHITE
-    elif color == 4:
-        color = BLUE
-
-    x = random.randrange(0, 450, 30)
-    y = random.randrange(0, 450, 30)
-    draw_rect(window, color, x, y)
-
-def draw_ligne(window, ligne):
-    x = 0
-    for case in ligne:
-        if case == 0:
-            color = BLACK
-        elif case == 1:
-            color = RED
-        elif case == 2:
-            color = GREEN
-        elif case == 3:
-            color = WHITE
-        elif case == 4:
-            color = BLUE
-        if x == 0:
-            x = 0
-        y = 0
-        draw_rect(window, color, x, y)
-        x = x + 30
-
 def draw_grille(window, grille):
     x = 0
     y = 0
@@ -79,40 +39,10 @@ def draw_wall(window, x, y):
 def draw_start(window, x, y):
     window.blit(start, (x, y))
 
-def draw_item(window, item_aiguille, item_tube, item_ether):
-    if item_aiguille[1] == True :
-        window.blit(aiguille, item_aiguille[0])
-    if item_tube[1] == True :
-        window.blit(tube, item_tube[0])
-    if item_ether[1] == True :
-        window.blit(ether, item_ether[0])
-
-def pop_items(grille):
-    liste_chemin = []
-    x = 0
-    y = 0
-    for liste in grille:
-        for case in liste:
-            if case == 1:
-                liste_chemin.append((x, y))
-            x = x + 30
-        x = 0
-        y = y + 30
-    index = random.randint(0, (len(liste_chemin) - 1))
-    coord_aiguille = liste_chemin[index]
-    del liste_chemin[index]
-    index = random.randint(0, (len(liste_chemin) - 1))
-    coord_tube = liste_chemin[index]
-    del liste_chemin[index]
-    index = random.randint(0, (len(liste_chemin) - 1))
-    coord_ether = liste_chemin[index]
-    return coord_aiguille, coord_tube, coord_ether
-
-
 
 class Player():
     def __init__(self):
-        self.image_player = pygame.image.load("Pictures/macgyver.bmp")
+        self.image_player = pygame.image.load("Pictures/Mcgyver.bmp")
         self.image_player.set_colorkey(WHITE)
         self.coord_player = [30, 30]
 
@@ -143,6 +73,9 @@ class Item():
         self.image_aiguille = pygame.image.load("Pictures/aiguille.bmp")
         self.image_tube = pygame.image.load("Pictures/tube.bmp")
         self.image_ether = pygame.image.load("Pictures/ether.bmp")
+        self.font_aiguille = pygame.image.load("Pictures/font_aiguille.bmp")
+        self.font_tube = pygame.image.load("Pictures/font_tube.bmp")
+        self.font_ether = pygame.image.load("Pictures/font_ether.bmp")
         self.image_ether.set_colorkey(WHITE)  
         self.image_aiguille.set_colorkey(WHITE)
         self.image_tube.set_colorkey(WHITE)
@@ -177,10 +110,17 @@ class Item():
     def draw_item(self, window):
         if self.aiguille[1] == True :
             window.blit(self.aiguille[0], self.aiguille[2])
+        else:
+            window.blit(self.font_aiguille, (455, 90))
         if self.tube[1] == True :
             window.blit(self.tube[0], self.tube[2])
+        else:
+            window.blit(self.font_tube, (455, 210))
         if self.ether[1] == True :
             window.blit(self.ether[0], self.ether[2])
+        else:
+            window.blit(self.font_ether, (455, 330))
+
 
     def check_item(self, coord_player):
         if coord_player == self.aiguille[2]:
@@ -201,13 +141,3 @@ def check_wall(grille, coord):
         return False
 
 
-
-def draw_random_grille(window):
-    [ligne_1, ligne_2, ligne_3, ligne_4, ligne_5, ligne_6, ligne_7, ligne_8, ligne_9, ligne_10, ligne_11, ligne_12, ligne_13, ligne_14, ligne_15] = [[] for i in range(15)]
-    grille = [ligne_1, ligne_2, ligne_3, ligne_4, ligne_5, ligne_6, ligne_7, ligne_8, ligne_9, ligne_10, ligne_11, ligne_12, ligne_13, ligne_14, ligne_15]
-
-    for ligne in grille:
-        while len(ligne) <= 15 :
-            x = random.randint(0, 1)
-            ligne.append(x)
-    draw_grille(window, grille)   
--- main.py
+++ main.py
@@ -8,25 +8,25 @@ import json
 
 pygame.init()
 
-size = (450 , 450)
+size = (550 , 450)
 
 pygame.display.set_caption("Projet 3: Aide Mc Gyver")
 background = pygame.image.load("Pictures/Background.bmp")
-wall = pygame.image.load("Pictures/wall.bmp")
-clock = pygame.time.Clock()
+cote = pygame.image.load("Pictures/font_cote.bmp")
 window = pygame.display.set_mode(size)
 window.blit(background, (0, 0))
+window.blit(cote, (450, 0))
 
-grille2 = json.load(open("grille.json"))
+grille = json.load(open("grille.json"))
 
 
 game = True
-func.draw_grille(window, grille2)
+func.draw_grille(window, grille)
 
 player = func.Player()
 player.generate_player(window)
 item = func.Item()
-item.generate_item(window, grille2)
+item.generate_item(window, grille)
 
 
 while game:
@@ -34,27 +34,23 @@ while game:
         if event.type == pygame.QUIT:
             game = False
         elif event.type == pygame.KEYDOWN:
-            window.blit(background, (0, 0))
-            item.check_item((player.coord_player[0],player.coord_player[1]))
-            func.draw_grille(window, grille2)
-            item.draw_item(window)
+            window.blit(background, (0, 0))            
+            func.draw_grille(window, grille)
+
             if event.key == K_RIGHT:
                 print("droite")
-                player.move_player(window, 30, 0, grille2)
+                player.move_player(window, 30, 0, grille)
             elif event.key == K_LEFT:
                 print("gauche")
-                player.move_player(window, -30, 0, grille2)
+                player.move_player(window, -30, 0, grille)
             elif event.key == K_UP:
                 print("haut")
-                player.move_player(window, 0, -30, grille2)
+                player.move_player(window, 0, -30, grille)
             elif event.key == K_DOWN:
                 print("bas")
-                player.move_player(window, 0, 30, grille2)
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            window.blit(background, (0, 0))
-            func.draw_random_grille(window)
-
+                player.move_player(window, 0, 30, grille)
+            item.check_item((player.coord_player[0],player.coord_player[1]))
+            item.draw_item(window)
 
     pygame.display.flip()
 
